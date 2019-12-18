ce8b7ac9b813d9e04a5932494aa02fb653a72325
junhan <wnsgsk789@gmail.com>
2019-12-18 20:00:33 +0900

    api 챔피언 정보 불러오는 기능 추가

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const url = require('url');
+const path = require('path');
+const fs = require('fs');
+
+const app = express();
+const dataPath = path.join(__dirname, '/../','testdata');
+const championList = JSON.parse(fs.readFileSync(path.join(dataPath, 'champion.json'))).data;
+
+/**
+ * 선택한 챔피언의 data 다운로드
+ * @param championId [string] 챔피언 id
+ */
+app.get('/champion/:championId', (req, res) => {
+  let championId = req.params.championId.charAt(0).toUpperCase()+req.params.championId.slice(1)
+  // res.send(championList).json();
+  if(championList[championId] == null) {
+    return res.status(400).json({error: 'Incorrect champion'});
+  }
+  const data = (fs.readFileSync(path.join(dataPath, `${championId}_data.json`), 'utf8'));
+  console.log(data);
+  return res.json(data);
+});
+
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
